5d7145df65a0b901ea2f816eb2eca31e2e871eb7
kaikySantos <info.kaiky@gmail.com>
2022-06-16 18:02:31 -0300

    feat: header finalized

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -49,6 +49,33 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Kaiky Santos</Text>
+            <Text color="gray.300" fontSize="small">carlos.kaiky62@gmail.com</Text>
+          </Box>
+
+          <Avatar size="md" name="Kaiky Santos" src="https://github.com/kaikySantos.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
