5b92acfffafc897aec95ed22d36e4bb592751e82
kaikySantos <info.kaiky@gmail.com>
2022-06-16 20:47:05 -0300

    feat: header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Kaiky Santos</Text>
-        <Text color="gray.300" fontSize="small">carlos.kaiky62@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Kaiky Santos</Text>
+          <Text color="gray.300" fontSize="small">carlos.kaiky62@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Kaiky Santos" src="https://github.com/kaikySantos.png" />
     </Flex>
