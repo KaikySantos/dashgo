ffbf0ecb109e8d6d358c2e2a90fab13dbc3e08b0
kaikySantos <info.kaiky@gmail.com>
2022-06-16 21:15:27 -0300

    feat: dashboard responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -73,7 +73,7 @@ export default function Dashboard() {
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -88,7 +88,7 @@ export default function Dashboard() {
             />
           </Box>
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
