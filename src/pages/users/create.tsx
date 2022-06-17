95d2f9540876da3705c7965283b9be4c9b74408f
kaikySantos <info.kaiky@gmail.com>
2022-06-16 21:34:45 -0300

    feat: create user responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,13 +12,13 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input
                 name="name"
                 label="Nome completo"
@@ -30,7 +30,7 @@ export default function CreateUser() {
               />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input
                 name="password"
                 label="Senha"
