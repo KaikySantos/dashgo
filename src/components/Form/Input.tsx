b9cedf15d9662ac758fe859d53d2a1dce814137b
kaikySantos <info.kaiky@gmail.com>
2022-06-16 22:43:45 -0300

    feat: react hook form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef, ForwardRefRenderFunction } from "react";
 import {
   FormControl,
   FormLabel,
@@ -10,7 +11,8 @@ interface InputProps extends ChackraInputProps {
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+  = ({ name, label, ...rest }, ref) => {
   return (
     <FormControl>
       { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
@@ -25,8 +27,11 @@ export function Input({ name, label, ...rest }: InputProps) {
           bgCOlor: "gray.900"
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,17 +31,20 @@ export default function Home() {
         p={8}
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input
             name="email"
             type="email"
             label="E-mail"
+            {...register('email')}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
+            {...register('password')}
           />
         </Stack>
 
@@ -36,6 +53,7 @@ export default function Home() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
