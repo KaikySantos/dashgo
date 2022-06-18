ccc771fec9930da3fb782725d1e56dbbca3a9454
kaikySantos <info.kaiky@gmail.com>
2022-06-18 20:51:37 -0300

    feat: react query - user list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        })
+      }
+    });
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -66,30 +79,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Kaiky Santos</Text>
-                        <Text fontSize="sm" color="gray.300">carlos.kaiky62@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>16 de Junho, 2022</Td> }
-                  </Tr>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Kaiky Santos</Text>
-                        <Text fontSize="sm" color="gray.300">carlos.kaiky62@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>16 de Junho, 2022</Td> }
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td>{user.createdAt}</Td> }
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
