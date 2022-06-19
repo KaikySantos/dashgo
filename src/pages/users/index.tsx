a2f817f14dbc0598165f3320f837a5a4fa3927f3
kaikySantos <info.kaiky@gmail.com>
2022-06-18 23:48:46 -0300

    feat: axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,12 +6,12 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+import { api } from '../../services/api';
 import { useQuery } from 'react-query';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+    const { data } = await api.get('users');
 
     const users = data.users.map(user => {
       return {
